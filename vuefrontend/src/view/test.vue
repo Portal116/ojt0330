a3de9b7f428fd4fdee9535d67a6bbdcb875b29fb
portal116 <tim950116@gmail.com>
2022-04-05 16:46:10 +0900

    gh end?

---
+++ vuefrontend/src/view/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+      <div v-html="test"></div>
+      <v-btn @click="htmltest">click</v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            test:""
+        }
+    },
+    methods:{
+        htmltest(){
+            this.test = "asdfasdfasdfasdf"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
