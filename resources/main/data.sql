be0da359f459184f7a0ebf637f1b9880e6ccc6e9
portal116 <tim950116@gmail.com>
2022-04-05 16:41:37 +0900

    Updates

--- resources/main/data.sql
+++
@@ -1,3 +0,0 @@
-insert into book(title, price, author, publisher) values('물고기는 존재하지 않는다', 17000, '룰루 밀러', '곰출판');
-insert into book(title, price, author, publisher) values('나를 알고 싶을 때 뇌과학을 공부합니다', 19800, '질 볼트 테일러', '윌북');
-insert into book(title, price, author, publisher) values('이기적 유전자', 20000, '리처드 도킨스', '을유문화사');
